40e454971ce9a90b251d097a9deed40e3cdebf16
katienotkathy <14117113+katienotkathy@users.noreply.github.com>
2023-01-24 20:28:24 -0500

    Complete basic structure. Pass first test.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringStart = '';
+    for (let i=0; i<=num; i++) {
+        if (i===0) continue;
+        else {
+            stringStart = stringStart.concat(string);
+        }
+    }
+    return stringStart
 };
 
 // Do not edit below this line
