3a7b9e0a9cac4d0dfd8a8141366f1c294abeeccf
katienotkathy <14117113+katienotkathy@users.noreply.github.com>
2023-01-25 20:34:09 -0500

    Complete exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    arrayString = string.split('');
+    let revString = arrayString.reverse();
+    return revString.join('');
 };
 
 // Do not edit below this line
