ae07bad29aef96778a750a4abf3ae0c53fda4930
katienotkathy <14117113+katienotkathy@users.noreply.github.com>
2023-01-27 19:44:38 -0500

    Complete exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    filteredArray = array.filter(x=>!args.includes(x));
+    return filteredArray;
 };
 
 // Do not edit below this line
