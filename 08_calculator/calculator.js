67053881769712956f7ba0c13115f424b26741bb
katienotkathy <14117113+katienotkathy@users.noreply.github.com>
2023-01-30 20:42:46 -0500

    Complete exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
+const add = function(x,y) {
+  return x + y;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+  return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(arrayToSum) {
+	if (arrayToSum.length == 0) return 0;
+  else {
+    let sum = 0;
+    arrayToSum.forEach(item => {
+      sum += item;
+    })
+    return sum;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(arrayToMultiply) {
+    let product = 1;
+    arrayToMultiply.forEach(item => {
+      product *= item;
+    })
+    return product;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x===0) return 1;
+  else {
+    let result = 1;
+    for (let i = 1; i <= x; i++) {
+      result *= i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
