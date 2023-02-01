2c5dd0d104180942458cec7e1b0579653dda6169
katienotkathy <14117113+katienotkathy@users.noreply.github.com>
2023-01-31 20:48:23 -0500

    Complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,31 @@
-const palindromes = function () {
+const palindromes = function (possiblePalindrome) {
+    var punctRE = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
+    var spaceRE = /\s+/g;
 
+    possiblePalindrome = possiblePalindrome.replace(punctRE,' ').replace(spaceRE,'');
+    possiblePalindrome = possiblePalindrome.toLowerCase();
+    
+    let stringLength = possiblePalindrome.length;
+
+    if (stringLength % 2 == 0) {
+        let stringFirstHalf = possiblePalindrome.substr(0,stringLength/2);
+        let tempSecondHalf = possiblePalindrome.substr(stringLength/2);
+        let splitString = tempSecondHalf.split("");
+        let reverseArray = splitString.reverse();
+        let stringSecondHalf = reverseArray.join("");
+
+        return stringFirstHalf === stringSecondHalf;
+    }
+    else {
+        let halfwayPoint = stringLength/2 - 0.5;
+        let stringFirstHalf = possiblePalindrome.substr(0,halfwayPoint);
+        let tempSecondHalf = possiblePalindrome.substr(halfwayPoint + 1);
+        let splitString = tempSecondHalf.split("");
+        let reverseArray = splitString.reverse();
+        let stringSecondHalf = reverseArray.join("");
+
+        return stringFirstHalf === stringSecondHalf;
+    }
 };
 
 // Do not edit below this line
